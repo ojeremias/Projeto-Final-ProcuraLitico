dd61e82093ab6cd760f477e9513d0739795f76b2
ojeremias <jeremias123verissimo@gmail.com>
2023-03-10 22:01:55 -0300

    inserindo arquivos base para o banco

---
+++ data_base/src/db.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require("sequelize");
+const sequelize = require("sequelize");
+
+const dbSequelize = new Sequelize("banco-deputados", "root", "mysql", {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports = dbSequelize;
